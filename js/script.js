f2de2c46cc1c3ce7eefacbce2ce4eedfe3bdc226
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-22 15:12:33 +0200

    add images and count

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-const app = new VTTCue(
+const app = new Vue(
     {
         el: '#app',
         data: {
-
-        }
+            count: 0,
+            images: [
+                'img/B01.jpg',
+                'img/B02.jpg',
+                'img/B03.jpg',
+                'img/B04.jpg',
+                'img/B05.jpg',
+                'img/B06.jpg',
+                'img/B07.jpg',
+                'img/B08.jpg'
+            ]
+        },
     }
-)
+);
